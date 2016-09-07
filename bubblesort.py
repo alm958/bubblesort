0d7078ef05fca57fcb1423e506cf8b0e0a12c71c
alm958 <alm958@gmail.com>
2016-09-07 14:11:53 -0400

    updated with version based on more efficient answer key solution

--- bubblesort.py
+++ bubblesort.py
@@ -1,16 +1,16 @@
 import random
 import time
-test = []
-for k in range(0,100):
-    test.append(int(round(random.random()*10000)))
-
+test = [random.randint(0,9) for k in range(10)]
 j = len(test)
+print test
 start_time = time.time()
 while j > 1 :
     for i in range(1,j):
         if test[i] < test[i-1] :
             (test[i], test[i-1]) = (test[i-1], test[i])
+        #print str(test)+'end of for loop'
     j -= 1
+    #print str(test) +'end of while loop'
 end_time = time.time()
 print test
 print 'sort execution time (miliseconds):'+str((end_time - start_time)*1000)
